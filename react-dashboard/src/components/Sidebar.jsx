0a2a83f53d0fdbdecabfe34bdb7788cc2cac5c8b
redhulkrko <old_blueyes@hotmail.com>
2026-02-16 21:51:31 +0000

    new pages commit

--- react-dashboard/src/components/Sidebar.jsx
+++ react-dashboard/src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <nav className="navbar align-items-start p-0 sidebar sidebar-dark accordion bg-gradient-primary navbar-dark">
       <div className="container-fluid d-flex flex-column p-0">
-        <a className="navbar-brand d-flex justify-content-center align-items-center m-0 sidebar-brand" href="#">
+        <a
+          className="navbar-brand d-flex justify-content-center align-items-center m-0 sidebar-brand"
+          href="#"
+        >
           <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-laugh-wink"></i>
           </div>
@@ -22,32 +25,48 @@ const Sidebar = () => {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/profile.html">
-              <i className="fas fa-user"></i>
-              <span>Profile</span>
+            <a className="nav-link" href="/films.html">
+              <i className="fas fa-film"></i>
+              <span>Films</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/table.html">
-              <i className="fas fa-table"></i>
-              <span>Table</span>
+            <a className="nav-link" href="/box-office.html">
+              <i className="fas fa-chart-line"></i>
+              <span>Box Office</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/login.html">
-              <i className="far fa-user-circle"></i>
-              <span>Login</span>
+            <a className="nav-link" href="/pages.html">
+              <i className="fas fa-file-alt"></i>
+              <span>Pages</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/register.html">
-              <i className="fas fa-user-circle"></i>
-              <span>Register</span>
+            <a className="nav-link" href="/archive.html">
+              <i className="fas fa-archive"></i>
+              <span>Archive</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/config.html">
+              <i className="fas fa-cog"></i>
+              <span>Config</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/trash.html">
+              <i className="fas fa-trash"></i>
+              <span>Trash</span>
             </a>
           </li>
         </ul>
         <div className="text-center d-none d-md-inline">
-          <button className="btn rounded-circle border-0" id="sidebarToggle" type="button"></button>
+          <button
+            className="btn rounded-circle border-0"
+            id="sidebarToggle"
+            type="button"
+          ></button>
         </div>
       </div>
     </nav>
